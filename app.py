87f902397e1d7196a3d19104dee0f0ae379a6b1c
Git Timm <daviddftimm@gmail.com>
2025-09-15 16:04:02 -0300

    fix path to 'vehicles.cvs'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-car_data = pd.read_csv('../vehicles.csv')  # lendo os dados
+car_data = pd.read_csv('./vehicles.csv')  # lendo os dados
 
 hist_button = st.button('Criar histograma.')  # cria um botão
 
